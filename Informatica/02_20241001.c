2d1a786d48e59bbce96fec51fd603e90182626c7
anasnair <anas.nair@galileiostiglia.edu.it>
2024-10-07 11:53:56 +0000

    p9p9p9

--- Informatica/02_20241001.c
+++ Informatica/02_20241001.c
@@ -4,25 +4,10 @@ delle cifre e stabilire se è divisibile per 3*/
 #include <stdio.h>
 
 int main() {
-    int numero, somma = 0, cifra;
-    printf("Inserisci un numero: ");
+    int numero=0;
+    int q, r;
+    printf("inserisci un numero: ");
     scanf("%d", &numero);
+    
 
-    int temp = numero; // Variabile temporanea per conservare il numero originale
-
-    // Calcolo della somma delle cifre
-    while (temp > 0) {
-        cifra = temp % 10; // Estrazione dell'ultima cifra
-        somma += cifra;    // Aggiunta della cifra alla somma
-        temp /= 10;        // Rimozione dell'ultima cifra
-    }
-
-    // Verifica se la somma delle cifre è divisibile per 3
-    if (somma % 3 == 0) {
-        printf("La somma delle cifre di %d è %d e questa è divisibile per 3.\n", numero, somma);
-    } else {
-        printf("La somma delle cifre di %d è %d e questa non è divisibile per 3.\n", numero, somma);
-    }
-
-    return 0;
 }
