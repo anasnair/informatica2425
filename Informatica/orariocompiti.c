6c72b53c18f8fc86ca46a85d1068df84e4e1d834
anasnair <anas.nair@galileiostiglia.edu.it>
2024-10-17 10:56:33 +0000

    22222222

--- Informatica/orariocompiti.c
+++ Informatica/orariocompiti.c
@@ -8,9 +8,9 @@ int main() {
     scanf("%d %d %d", &ore2, &minuti2, &secondi2);
 
     if (ore1 < ore2 || (ore1 == ore2 && minuti1 < minuti2) || (ore1 == ore2 && minuti1 == minuti2 && secondi1 < secondi2)) {
-        printf("Il primo orario (%02d:%02d:%02d) viene prima del secondo (%02d:%02d:%02d).\n", ore1, minuti1, secondi1, ore2, minuti2, secondi2);
+        printf("Il primo orario (%d:%d:%d) viene prima del secondo (%d:%d:%d).", ore1, minuti1, secondi1, ore2, minuti2, secondi2);
     } else {
-        printf("Il secondo orario (%02d:%02d:%02d) viene prima del primo (%02d:%02d:%02d).\n", ore2, minuti2, secondi2, ore1, minuti1, secondi1);
+        printf("Il secondo orario (%d:%d:%d) viene prima del primo (%d:%d:%d).", ore2, minuti2, secondi2, ore1, minuti1, secondi1);
     }
 
     return 0;
